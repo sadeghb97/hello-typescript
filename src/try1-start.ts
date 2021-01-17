424ac3a6521adea628915bce71a28f4b87965e99
Sadegh Bagherzadeh <sadeghb97@gmail.com>
2021-01-17 19:10:51 +0330

    start

---
+++ src/try1-start.ts
@@ -0,0 +1,18 @@
+/* Start using typescript:
+npm install -g typescript
+tsc --init --> in project directory
+git init
+npm init
+*/
+
+/*
+file haye ts mostaghim dar morurgar ejra nakhahand shod. file haye tse ma ba compilere
+typescript ebteda be js tabdil mishavand va sepas ma file bundle shode ra dar morurgar import mikonim
+tanzimate compile typescript darune tsconfig gharar darad.
+masalan ma optione marbut be hazfe comment ha az file compile shode ra fa'al kardim va hamchenin
+ma masire source proje ra ./src va mahale build ra ./build gharar dade im 
+dar natije code haye compile shode dar directorie ./build gharar khahand gereft
+ba dasture tsc (typescript compile) code haye tsc compile
+*/
+
+console.log("Hello Typescript!")
